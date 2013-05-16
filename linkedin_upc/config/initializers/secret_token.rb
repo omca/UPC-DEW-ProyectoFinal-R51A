f0ccb666473e7379e2f3754f70a05b5c9060b96a
Omar Calderon <calderonomar48@gmail.com>
2013-05-16 07:51:51 -0500

    Subiendo el proyecto linkedin

---
+++ linkedin_upc/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LinkedinUpc::Application.config.secret_key_base = '531baefc1bb8cc919f5941923745f79cc1d369ea8c43f990770eb7ca9e879adca14b367cfbdaecd88b6cfabfecc4c2320c1706dd51bda5092a2550af23098172'
---
+++ linkedin_upc/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LinkedinUpc::Application.config.session_store :cookie_store, key: '_linkedin_upc_session'
