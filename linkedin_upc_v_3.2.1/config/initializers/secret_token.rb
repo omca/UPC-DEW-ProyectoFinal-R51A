ccda8da7cf29de0df2d2e8c4b89feb772603556a
Omar Calderon <calderonomar48@gmail.com>
2013-05-28 21:05:26 -0500

    SUBIENDO NUEVA VERSION DE PROYECTO, VERSION 3.2.1

---
+++ linkedin_upc_v_3.2.1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinUpcV321::Application.config.secret_token = '8e561eb6216f15b435939429d4cc13cd0669e88049f0090c28aaadceed27823535283288b80f110853167614d1e8954e7ede36b6f541727918c2bb7ae7a37489'
