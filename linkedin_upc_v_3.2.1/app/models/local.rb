d6baabb0d90febb2fa99ab648ff21c9a851a74b7
juan <juanquillatupaamaya@gmail.com>
2013-06-01 10:48:06 -0500

    integracion con google maps

--- linkedin_upc_v_3.2.1/app/models/local.rb
+++ linkedin_upc_v_3.2.1/app/models/local.rb
@@ -1,6 +1,7 @@
 class Local < ActiveRecord::Base
-  attr_accessible :address, :district_id, :maps_x, :maps_y, :name, :phone
-
-  belongs_to :district
-  has_many :concerts
+  attr_accessible :address, :district_id, :gmaps, :latitude, :longitude, :name, :phone
+  acts_as_gmappable
+  def gmaps4rails_address
+	address
+  end
 end
--- linkedin_upc_v_3.2.1/test/functional/locals_controller_test.rb
+++ linkedin_upc_v_3.2.1/test/functional/locals_controller_test.rb
@@ -18,7 +18,7 @@ class LocalsControllerTest < ActionController::TestCase
 
   test "should create local" do
     assert_difference('Local.count') do
-      post :create, local: { address: @local.address, district_id: @local.district_id, maps_x: @local.maps_x, maps_y: @local.maps_y, name: @local.name, phone: @local.phone }
+      post :create, local: { address: @local.address, district_id: @local.district_id, gmaps: @local.gmaps, latitude: @local.latitude, longitude: @local.longitude, name: @local.name, phone: @local.phone }
     end
 
     assert_redirected_to local_path(assigns(:local))
@@ -35,7 +35,7 @@ class LocalsControllerTest < ActionController::TestCase
   end
 
   test "should update local" do
-    put :update, id: @local, local: { address: @local.address, district_id: @local.district_id, maps_x: @local.maps_x, maps_y: @local.maps_y, name: @local.name, phone: @local.phone }
+    put :update, id: @local, local: { address: @local.address, district_id: @local.district_id, gmaps: @local.gmaps, latitude: @local.latitude, longitude: @local.longitude, name: @local.name, phone: @local.phone }
     assert_redirected_to local_path(assigns(:local))
   end
 
