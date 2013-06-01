c9b3db7e584f08773948024579dd33be64b410c4
Patricia Trinidad <patricia.trileq@gmail.com>
2013-06-01 09:56:33 -0500

    BD final

--- linkedin_upc_v_3.2.1/app/models/band.rb
+++ linkedin_upc_v_3.2.1/app/models/band.rb
@@ -1,9 +1,9 @@
 class Band < ActiveRecord::Base
   attr_accessible :name, :style_id, :user_id
 
-  belongs_to :user
   belongs_to :style
   belongs_to :concert
+  has_many :users_bands
   validates_presence_of :name, :user_id, :style_id
   validates_uniqueness_of :name
 end
