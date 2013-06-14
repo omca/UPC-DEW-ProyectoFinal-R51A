e1b78bb2eb5500c75a336301b7cf0fb7ac568248
jquillatupa <juanquillatupaamaya@hotmail.com>
2013-06-14 00:37:28 -0500

    actualizando

--- linkedin_upc_v_3.2.1/app/models/local.rb
+++ linkedin_upc_v_3.2.1/app/models/local.rb
@@ -1,5 +1,8 @@
 class Local < ActiveRecord::Base
-  attr_accessible :address, :district_id, :gmaps, :latitude, :longitude, :name, :phone
+attr_accessible :address, :district_id, :gmaps, :latitude, :longitude, :name, :phone
+  
+	belongs_to :district
+	has_many :concerts
   acts_as_gmappable
   def gmaps4rails_address
 	address
